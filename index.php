71252f78161762152c19566783c2884c27d200d0
CristianHourcade <hourcadecristian@gmail.com>
2017-08-23 23:08:23 -0300

    Agregando los primeros archivos al repositorio

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/estilo.css">
+	<title>Tabla compu</title>
+</head>
+<body>
+<div class="container-fluid  mx-0">
+
+<div class="row cf">
+<div class="col-12">
+	<p class="text-white display-4 h1 mx-auto"><b>Presupuestos de nuestro servicio</b></p>
+	</div>
+	<div class="col-12"><hr style="border:1px solid white;"></div>
+</div>
+	
+	
+<div class="row">
+	
+	<div class="offset-1 col-6 mb-4">
+<table class="table table-bordered mt-5">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Basico</th>
+      <th>Normal</th>
+      <th>Premium</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Precio</th>
+      <td>$</td>
+      <td>$</td>
+      <td>$</td>
+    </tr>
+    <tr>
+      <th scope="row">Paginas</th>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row">Hosting</th>
+      <td>.com</td>
+      <td>.com</td>
+      <td>.com</td>
+    </tr>
+    <tr>
+      <th scope="row">Soporte</th>
+      <td>¿?</td>
+      <td>¿?</td>
+      <td>¿?</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class="col-4">
+	<img src="images/homer.png">
+	</div>
+</div>
+</div>
+<div class="container-fluid mx-0">
+
+<div class="row ">
+<div class="offset-2 col-8 bg-black py-3" style="border-radius: 50px;">
+	<p class="h5 text-white text-center">Copyright © Todos los Derechos Reservados</p>
+</div>
+</div>
+</div>
+</body>
+</html>
